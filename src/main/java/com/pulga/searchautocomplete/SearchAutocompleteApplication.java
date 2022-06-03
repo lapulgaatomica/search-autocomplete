e65afaa19c6222d1f9bb43e4f773a87900cee7a9
akindele odedoyin <akindeleodedoyin@gmail.com>
2022-06-03 10:53:46 +0100

    initial commit

---
+++ src/main/java/com/pulga/searchautocomplete/SearchAutocompleteApplication.java
@@ -0,0 +1,13 @@
+package com.pulga.searchautocomplete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchAutocompleteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchAutocompleteApplication.class, args);
+	}
+
+}
